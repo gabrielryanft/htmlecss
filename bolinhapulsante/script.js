f31c2a3180c98e6d16036958147fb12db1926cc1
Gabriel Ryan <135415258+gabrielryanft@users.noreply.github.com>
2023-11-25 19:16:02 -0300

    Update bolinha.html

---
+++ bolinhapulsante/script.js
@@ -0,0 +1,13 @@
+var c_width = document.body.clientWidth
+var c_height = document.body.clientHeight
+const elemento = document.querySelector("main")
+
+console.log(c_height, c_width)
+
+if (c_width < c_height){
+    elemento.style.setProperty("--m_height", "10vw") 
+    elemento.style.setProperty("--m_width", "10vw") 
+} else {
+    elemento.style.setProperty("--m_height", "10vh") 
+    elemento.style.setProperty("--m_width", "10vh") 
+}
