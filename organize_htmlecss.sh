00eb0fe09e0a3bf95e1188a7c9722f43f94f292b
gabrielryanft <135415258+gabrielryanft@users.noreply.github.com>
2024-09-09 10:54:43 -0300

    fixed organize_htmlecss.sh

--- organize_htmlecss.sh
+++ organize_htmlecss.sh
@@ -1,1 +1,9 @@
-/home/po/scripts/organize_htmlecss.sh
+#!/bin/bash
+
+ls | grep -v -e "README.md" -e "LICENSE" -e "organize_htmlecss.sh" > exists.txt
+printf "# HTMLeCSS\n" > README.md
+while read name
+do
+	printf "<a href='https://gabrielryanft.github.io/htmlecss/$name/index.html' target='_blank' rel='next'>$name</a><br/>\n" >> README.md
+
+done < exists.txt
