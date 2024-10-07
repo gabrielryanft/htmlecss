7a02a83b8a7eac0b235e54915029bbba84cde5ff
gabrielryanft <135415258+gabrielryanft@users.noreply.github.com>
2024-10-07 06:41:55 -0300

    update galeriadefotos

--- animacaoletrapalavra/script.js
+++
@@ -1,9 +0,0 @@
-document.body.onload = onda()
-
-function onda(){
-    const div_letter = document.querySelector("div")
-
-    for(i=0;i<(div_letter.children.length - 1);i++){
-        div_letter.children[i].style.animationDelay = `${(i / 3) * 1}s`
-    }
-}
