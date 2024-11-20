a806cebad86d92d6036d0bbcebde98271fb7abb7
gabrielryanft <135415258+gabrielryanft@users.noreply.github.com>
2024-11-20 20:57:30 -0300

    Update organize_htmlecss.sh
    Update organize_htmlecss.sh to use temp files

--- organize_htmlecss.sh
+++ organize_htmlecss.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-ls | grep -v -e "README.md" -e "LICENSE" -e "organize_htmlecss.sh" -e "exists.txt" > exists.txt
+tmp_file=$(mktemp)
+ls | grep -v -e "README.md" -e "LICENSE" -e "organize_htmlecss.sh" > $tmp_file
 printf "# HTMLeCSS\n" > README.md
 while read name
 do
 	printf "<a href='https://gabrielryanft.github.io/htmlecss/$name/index.html' target='_blank' rel='next'>$name</a><br/>\n" >> README.md
 
-done < exists.txt
+done < $tmp_file
