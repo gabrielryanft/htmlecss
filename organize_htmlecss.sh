4424b5f90a6726366af4f1a8b73a9175e2f29aba
Gabriel Ryan <gabrielryanf.t@gmail.com>
2025-06-13 02:44:36 -0300

    make index cooler

--- organize_htmlecss.sh
+++ organize_htmlecss.sh
@@ -1,27 +1,36 @@
 #!/bin/bash
 
 tmp_file=$(mktemp)
-ls | grep -v -e "README.md" -e "LICENSE" -e "organize_htmlecss.sh" -e "index.html" > $tmp_file
+ls | grep -v -e "README.md" -e "LICENSE" -e "organize_htmlecss.sh" -e "index.html" -e "index_things" > $tmp_file
 printf "# HTMLeCSS\n" > README.md
-printf '
-<!DOCTYPE html>
+printf '<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>HTMLeCSS - GabrielRyanFT </title>
+    <link rel="stylesheet" href="./index_things/style.css">
 </head>
 <body>
 <h1>HTMLeCSS</h1>
 <p>A list of my tests with HTML and CSS</p>
+
+<main>
 ' > index.html
 while read name
 do
 	printf "<a href='https://gabrielryanft.github.io/htmlecss/$name/index.html' target='_blank' rel='next'>$name</a><br/>\n" >> README.md
-	printf "<a href='https://gabrielryanft.github.io/htmlecss/$name/index.html' target='_blank' rel='next'>$name</a><br/>\n" >> index.html
+	printf "
+<a href='https://gabrielryanft.github.io/htmlecss/$name/index.html' target='_blank' rel='next' class='card'>
+    <div class='image'>
+	<img src='./index_things/images/${name}.png' alt='Image of website'>
+    </div>
+    <p>$name</p>
+</a>" >> index.html
 
 done < $tmp_file
 printf "
+</main>
 </body>
 </html>
 " >> index.html
